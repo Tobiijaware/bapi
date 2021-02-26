7645f112e9fc1a7e690fd0e57219130a876318f1
Tobiijaware <62187913+Tobiijaware@users.noreply.github.com>
2021-02-26 05:19:10 -0800

    Update LoanUser.php

--- app/Models/LoanUser.php
+++ app/Models/LoanUser.php
@@ -14,6 +14,9 @@ class LoanUser extends Model
     protected $primaryKey = 'id';
 
     protected $fillable = [
-        'users_id', 'loan_id', 'referrence_code', 'due_date', 'amount','profee', 'interest', 'rep','tranch','start','stop','terminate','penalty','rate','days','status'
+        'users_id', 'loan_id', 'referrence_code', 'due_date', 'amount','profee', 'interest',
+        'rep','tranch','start','stop','terminate','penalty','rate','days','status','surname','othername','phone','email','gender',
+        'marital_status','education','homeaddress','state', 'city', 'emp_status', 'dob', 'bvn',
+        'work_details','guarantor_name','rel_with_guar','number_of_guar', 'loan_purpose', 'how_did_u_find'
     ];
 }
